71ab9e254eaa1a90482f95910bb07845b6bcb4f0
Ragavan Ravichandran <raviragavan98@gmail.com>
2025-12-05 09:29:38 +0530

    create inter,ediate models

---
+++ models/fact/fct_reviews.sql
@@ -0,0 +1,24 @@
+{{
+    config(
+        materialized ='incremental',
+        on_schema_change = 'fail' 
+        )
+}}
+
+with src_reviews AS (
+    select * from {{ ref('src_reviews') }}
+)
+
+select 
+* from src_reviews
+where review_text is not null
+{% if is_incremental() %}
+  {% if var("start_date", False) and var("end_date", False) %}
+    {{ log('Loading ' ~ this ~ ' incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True) }}
+    AND review_date >= '{{ var("start_date") }}'
+    AND review_date < '{{ var("end_date") }}'
+  {% else %}
+    AND review_date > (select max(review_date) from {{ this }})
+    {{ log('Loading ' ~ this ~ ' incrementally (all missing dates)', info=True)}}
+  {% endif %}
+{% endif %}
